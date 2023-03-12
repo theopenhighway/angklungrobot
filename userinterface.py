104308a221b39578502604074f2d5deaf02df70a
theopenhighway <earleen.525190010@stu.untar.ac.id>
2023-03-12 23:14:37 +0700

    created 3 classes for each window

--- userinterface.py
+++ userinterface.py
@@ -1,24 +1,77 @@
 import tkinter as tk
+from tkinter import ttk
 import serial
 from time import sleep
-from PIL import ImageTk
+# from PIL import ImageTk
 from tkinter.filedialog import askopenfilename
 
+TITLEFONT = ("calibri", 30)
+CAPTIONFONT = ()
+BUTTONFONT = ()
+
+def stopCode():
+    exit
+
+class AngklungApp(tk.Tk):
+     
+    # __init__ function for class AngklungApp
+    def __init__(self, *args, **kwargs):
+         
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
+         
+        # creating a container
+        container = tk.Frame(self) 
+        container.pack(side = "top", fill = "both", expand = True)
+  
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+  
+        # initializing frames to an empty array
+        self.frames = {} 
+  
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in (mainMenu, modeManual, modeOtomatis):
+  
+            frame = F(container, self)
+  
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with
+            # for loop
+            self.frames[F] = frame
+  
+            frame.grid(row = 0, column = 0, sticky ="nsew")
+  
+        self.show_frame(mainMenu)
+  
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class mainMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        welcomesign = tk.Label(self, text="SELAMAT DATANG", anchor="n", font=TITLEFONT)
+        
+        modeManual_btn = ttk.Button(self, text ="Mode Manual",
+        command = lambda : controller.show_frame(modeManual))
+
+        modeOtomatis_btn = ttk.Button(self, text ="Mode Otomatis",
+        command = lambda : controller.show_frame(modeOtomatis))
+
+class modeManual(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+class modeOtomatis(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
 
-def main():
-    ser = serial.Serial('/dev/ttACM0',9600)
-    ser.flush()
-
-    # initiallize app
-    root = tk.TK()
-
-    # screen dimension
-    root.geometry('1024x600')
-
-    #run app
-    root.mainloop()  
-    
-if __name__ == '__main__':
-    main()
-    
+root = AngklungApp()
+root.title("Angklung")
+root.geometry('1024x600')
+root.mainloop()
