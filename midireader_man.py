79d52be26e9afc265a8fa9d81d4b9190ea0c8e48
theopenhighway <earleen.525190010@stu.untar.ac.id>
2023-03-15 12:19:17 +0700

    improved readability

--- midireader_man.py
+++ midireader_man.py
@@ -1,14 +1,14 @@
 import time
 import rtmidi
 from rtmidi.midiutil import open_midiinput
-from rtmidi.midiconstants import NOTE_ON, NOTE_OFF, TIME_SIGNATURE, TEMPO, END_OF_TRACK
+# from rtmidi.midiconstants import NOTE_ON, NOTE_OFF, TIME_SIGNATURE, TEMPO, END_OF_TRACK
 import serial
 # import RPi.GPIO as GPIO
 # import spidev
 # from lib_nrf24 import NRF24
 
-#ser = serial.Serial('/dev/ttACM0',9600)
-#ser.flush()
+ser = serial.Serial('/dev/ttyUSB0',9600, timeout=1)
+ser.flush()
 
 intr = rtmidi.MidiIn()
 ports = intr.get_ports()
@@ -33,22 +33,16 @@ while True:
         else:
             motorNO = (notes % 12)  + 1
 
-        listmes = [notestat, motorNO, velocity, int(dt * 1000)]
-        listmes = bytes(listmes)
-
-        # motorNOstr = str(motorNO) + '\n'
         if command == '0x90':
-            #ser.write("on \n")
-            #ser.write(motorNOstr.encode('ascii'))
-            print(listmes)
+            texterON = 'Motor ' + str(motorNO) + ' on\n'
+            print(texterON + 'is sent')
+            ser.write(texterON.encode('ascii'))
             #print(f"{command} {msg[1:]}\t| dt = {dt:.2f}")
         elif command == '0x80':
-            #ser.write("off \n")
-            #ser.write(motorNOstr.encode('ascii'))
-            print(motorNO)
-            #ser.write(int(dt))
+            texterOFF = 'Motor ' + str(motorNO) + ' off\n'
+            print(texterOFF + 'is sent')
+            ser.write(texterOFF.encode('ascii'))
             #print(f"{command} {msg[1:]}\t| dt = {dt:.2f}")
-        # print(msgde)
     else:
         time.sleep(0.001)
 
